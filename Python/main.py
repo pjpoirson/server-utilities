89978cc92257597005214cc95c7a139416ab356c
Paul-Jean <pjpoirson@gmail.com>
2022-09-30 16:46:04 +0200

    :memo: New examples

--- Python/main.py
+++ Python/main.py
@@ -1,10 +1,32 @@
 import utils
+import os.path
+from os import path
 
-i = 1
-while i <= 12:
-    m = (str(i), "0"+str(i))[i<10]
-    utils.delete(r"/2017/"+m+"/")
-    # source_folder = r"/2017/"+m+"/"
-    # destination_folder = r"/2017/"
-    # move_all_files(source_folder, destination_folder)
-    i += 1
+# Create month / year folders and move files
+# year = "2022"
+# m = 1
+# while m <= 12:
+#     month = (str(m), "0"+str(m))[m<10]
+#     source_folder = r"/home/"+year+"/"
+#     destination_folder = r"/home/"+year+"/"+year+month+"/"
+#     if not path.exists(destination_folder):
+#         utils.mkdir(destination_folder)
+#     if (path.exists(source_folder)):
+#         utils.move_files_with_pattern(source_folder, destination_folder, "*"+year+month+"*.pdf")
+#     m += 1
+
+# Browse month and day folders
+# Move files and delete old directory
+# m = 1
+# while m <= 12:
+#     d = 1
+#     month = (str(m), "0"+str(m))[m<10]
+#     while d <= 31:
+#         day = (str(d), "0"+str(d))[d<10]
+#         source_folder = r"/home/2017/2017"+month+"/factures-2017"+month+day+"/"
+#         destination_folder = r"/home/2017/2017"+month+"/"
+#         if path.exists(source_folder):
+#             utils.move_all_files(source_folder, destination_folder)
+#             utils.delete(source_folder)
+#         d += 1
+#     m += 1
